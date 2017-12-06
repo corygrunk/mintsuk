36c66435561eb1b442089b6157ae82f9d5285372
Cory Grunkemeyer <cory.grunkemeyer@robotsandpencils.com>
2017-12-06 15:34:07 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxMoveSpeed = 2f;
+    private float moveSpeed;
+
+    Animator animator;
+
+	void Start () {
+
+        animator = GetComponent<Animator>();
+        moveSpeed = 0;
+	}
+	
+	void Update () {
+
+        // Move (Keyboard)
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveSpeed = maxMoveSpeed;
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveSpeed = maxMoveSpeed;
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        // Stop Moving (Keyboard)
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            moveSpeed = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            moveSpeed = 0;
+        }
+
+        // Animator
+        animator.SetFloat("Speed", moveSpeed);
+	}
+}
